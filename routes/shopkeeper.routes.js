a3b6fd9dbd9ac32bb710328885a7d52245981f77
Navediqbal123 <navedahmad9012@gmail.com>
2025-12-13 08:33:40 +0530

    Add shopkeeper routes for becoming and dashboard

--- routes/shopkeeper.routes.js
+++ routes/shopkeeper.routes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import shopkeeperOnly from "../middleware/shopkeeper.js";
+import { becomeShopkeeper, shopkeeperDashboard } from "../controllers/shopkeeperController.js";
 
+const router = express.Router();
+
+router.post("/become-shopkeeper", becomeShopkeeper);
+router.get("/dashboard", shopkeeperOnly, shopkeeperDashboard);
+
+export default router;
